19554e997320a5c951e3c80f55f6366b8b673544
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-03-26 16:09:05 +0300

    added OrmWithoutBLL

---
+++ OrmWithout.BusinessLogic/Abstract/IOrmWithoutBLL.cs
@@ -0,0 +1,18 @@
+﻿using OrmWithout.Models.Entities;
+using OrmWithout.Models.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OrmWithout.BusinessLogic.Abstract
+{
+    public interface IOrmWithoutBLL
+    {
+        Task<IList<SalesHistoryModel>> GetSalesHistory();
+        Task<StoresProfitModel> GetProfitStores(string storeName);
+        Task<StoresProfitModel> GetMostProfitStore();
+        Task<BestSellerProductModel> GetBestSellerProduct();
+        Task<bool> AddInventorySales(InventorySales inventorySales);
+        Task<InventorySales> UpdateInventorySales(InventorySales inventorySales);
+        Task<bool> DeleteInventorySale(int inventoryId);
+    }
+}
---
+++ OrmWithout.BusinessLogic/Services/OrmWithoutBLL.cs
@@ -0,0 +1,54 @@
+﻿using OrmWithout.BusinessLogic.Abstract;
+using OrmWithout.DataAccess.Abstract;
+using OrmWithout.Models.Entities;
+using OrmWithout.Models.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OrmWithout.BusinessLogic.Services
+{
+    public class OrmWithoutBLL : IOrmWithoutBLL
+    {
+        private readonly IOrmWithoutDAL _orm;
+
+        public OrmWithoutBLL(IOrmWithoutDAL orm)
+        {
+            _orm = orm;
+        }
+
+        public async Task<bool> AddInventorySales(InventorySales inventorySales)
+        {
+            return await _orm.AddInventorySales(inventorySales);
+        }
+
+        public async Task<bool> DeleteInventorySale(int inventoryId)
+        {
+            return await _orm.DeleteInventorySale(inventoryId);
+        }
+
+        public async Task<BestSellerProductModel> GetBestSellerProduct()
+        {
+            return await _orm.GetBestSellerProduct();
+        }
+
+        public async Task<StoresProfitModel> GetMostProfitStore()
+        {
+            return await _orm.GetMostProfitStore();
+        }
+
+        public async Task<StoresProfitModel> GetProfitStores(string storeName)
+        {
+            return await _orm.GetProfitStores(storeName);
+        }
+
+        public async Task<IList<SalesHistoryModel>> GetSalesHistory()
+        {
+            return await _orm.GetSalesHistory();
+        }
+
+        public async Task<InventorySales> UpdateInventorySales(InventorySales inventorySales)
+        {
+            return await _orm.UpdateInventorySales(inventorySales);
+        }
+    }
+}
